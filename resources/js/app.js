376d09799234ca01753bbcb301f3691e6550125d
erme2 <me@erme2.com>
2020-04-10 17:52:10 +0100

    vue not working

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 require('./bootstrap');
+
+// setting up jQuery
+import $ from 'jquery';
+window.$ = window.jQuery = $;
+
+// setting up Vue
+import Vue from 'vue';
+new Vue({
+    el: '#app',
+});
