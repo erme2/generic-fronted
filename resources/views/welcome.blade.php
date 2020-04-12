3db8fcd8237f0b0886ef8a325b2cb7bdecca99d6
erme2 <me@erme2.com>
2020-04-12 20:14:28 +0100

    ...

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -270,7 +270,7 @@
                         data: [15339, 21345, 18483, 24003, 23489, 24092, 12034],
                         lineTension: 0,
                         backgroundColor: 'transparent',
-                        borderColor: '#007bff',
+                        borderColor: '#007bf1',
                         borderWidth: 4,
                         pointBackgroundColor: '#007bff'
                     }]
