8ace8242a11fc4826791f8c71d2d55fe0ac2b1ee
Arduino Di Giosia <arduino.digiosia@moreniche.com>
2020-04-12 19:56:18 +0100

    ...

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="description" content="">
-        <title>Rico</title>
+        <title>{{ $title }}</title>
 
         <link href="/css/app.css" rel="stylesheet">
 
